0bf36ccfc8ee7b68f64a9158ee394546ada7eba7
Yai <me@rapipat.dev>
2024-06-22 23:49:55 +0700

    Add bk formula

--- Formula/bk.rb
+++ Formula/bk.rb
@@ -2,7 +2,7 @@ class Bk < Formula
   desc "A simple backup command"
   homepage "https://github.com/yzi-afk/bk"
   url "https://github.com/yzi-afk/bk/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "2f784095d4338e51c326f1ac1d0548a4dc8073d3"
+  sha256 "6250dc459590e2d24669541a174250a5fee65defbe9a22be814bce7025755035"
   version "1.0.0"
 
   def install
